3e4f36075abe23585f9894eb6040b40804bd491d
agilestring <atakistanley@gmail.com>
2020-08-25 20:50:18 +0100

    Initial commits, app setup

---
+++ dropoutgeeks/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <p>Hi Cohort2 😃</p>
+    </div>
+  );
+}
+
+export default App;
